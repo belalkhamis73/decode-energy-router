1d57f2c0e3b80c29580368b2035edd0cdf35a545
belalkhamis73 <belal.khamis73@gmail.com>
2026-01-19 19:05:53 +0200

    Update __init__.py

--- physics-core/equations/__init__.py
+++ physics-core/equations/__init__.py
@@ -4,11 +4,20 @@ Exposes the governing laws of the microgrid as importable modules.
 """
 
 from .swing_equation import SwingEquation
-from .power_flow import PowerFlowEquation
+from .power_flow import PowerFlowEquation, validate_kirchhoff
 from .battery_thermal import BatteryThermalEquation
+from .battery_health import BatteryHealthEquation, BatteryParams
+from .energy_router import EnergyRouter, SourceState
+from .grid_estimator import GridEstimator
 
 __all__ = [
     "SwingEquation",
     "PowerFlowEquation",
-    "BatteryThermalEquation"
+    "validate_kirchhoff",
+    "BatteryThermalEquation",
+    "BatteryHealthEquation",
+    "BatteryParams",
+    "EnergyRouter",
+    "SourceState",
+    "GridEstimator"
 ]
