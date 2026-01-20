35d62d65a16ae66c0995a01040cdc68e8bf46e3c
belalkhamis73 <belal.khamis73@gmail.com>
2026-01-20 14:22:01 +0200

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,6 @@
 
 from pydantic_settings import BaseSettings
 from typing import Dict, Any, Optional
-from backend.app.core.config import settings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "D.E.C.O.D.E."
